d59b8c226426e2fc576c88df3e61cec96d6e2de8
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-08-09 14:30:56 -0400

    add command parser and handler to bot.js

--- bot.js
+++ bot.js
@@ -15,3 +15,22 @@ bot.on("ready", () => {
 bot.on("debug", (err) => console.info(err.grey));
 bot.on("warn", (err) => console.warn(err.yellow));
 bot.on("error", (err) => console.error(err.red));
+
+bot.on("message", (message) => {
+  // check if message doesn't contain command prefix or if author is bot
+  if (!message.content.startsWith(config.prefix) || message.author.bot) {
+    return;
+  }
+
+  // slice out arguments from command string
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  // shift command out of args array, lowercase to make case-insensitive
+  const command = args.shift().toLowerCase().replace("/", "");
+
+  try {
+    const commandFile = require(`./commands/${command}.js`);
+    commandFile.run(bot, message, args);
+  } catch (error) {
+    console.error(error.red);
+  }
+});
