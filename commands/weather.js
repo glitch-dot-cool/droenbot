06d02f18cf64f8bc00bf8f52611480202425a67d
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-08-10 23:38:03 -0400

    make weather command a rich msg, add fields/details

--- commands/weather.js
+++ commands/weather.js
@@ -1,21 +1,36 @@
+const Discord = require("discord.js");
 const { weatherstack_token } = require("../config.json");
 const axios = require("axios");
 
 exports.run = async (bot, message, args) => {
   // process input args to URL friendly
-  const location = encodeURI(args.join(" "));
+  const query = encodeURI(args.join(" "));
 
   // await server response from query derived from input args & token from config.json
   const response = await axios.get(
-    `http://api.weatherstack.com/current?access_key=${weatherstack_token}&query=${location}`
+    `http://api.weatherstack.com/current?access_key=${weatherstack_token}&query=${query}`
   );
 
   // pull off properties from server response
-  const condition = response.data.current.weather_descriptions[0];
-  const temp = response.data.current.temperature;
-  const location_response = response.data.location.name;
+  const location = response.data.location.name;
+  const observation_time = response.data.location.localtime;
+  const current = response.data.current;
+  const icon = current.weather_icons[0];
+  const condition = current.weather_descriptions[0];
+  const temp = current.temperature;
+  const { wind_speed, humidity, cloudcover } = current;
 
-  message.channel.send(
-    `The weather is ${condition} and ${temp} degrees celcius in ${location_response}`
-  );
+  const embed = new Discord.MessageEmbed()
+    .setTitle(
+      `The weather is ${condition} and ${temp} degrees celcius in ${location}`
+    )
+    .setImage(icon)
+    .addFields(
+      { name: "wind speed", value: wind_speed },
+      { name: "humidity", value: `${humidity}%` },
+      { name: "cloud cover", value: `${cloudcover}%` }
+    )
+    .setFooter(`observation time: ${observation_time} (local)`);
+
+  message.channel.send(embed);
 };
