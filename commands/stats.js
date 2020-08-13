1421b70270fb79cba56e3ab81dfbafe707d3f406
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-08-12 22:35:56 -0400

    add last msg date and top role color to stats command

--- commands/stats.js
+++ commands/stats.js
@@ -5,6 +5,9 @@ exports.run = (bot, message, args) => {
   const author_id = message.author.id;
   const guild_id = message.guild.id;
   const joined_at = new Date(message.member.joinedAt);
+  const last_msg = message.member.lastMessage.createdAt;
+  const color = bot.guilds.resolve(guild_id).members.resolve(author_id).roles
+    .color.color;
   const avatar = bot.guilds
     .resolve(guild_id)
     .members.resolve(author_id)
@@ -15,11 +18,13 @@ exports.run = (bot, message, args) => {
   const embed = new Discord.MessageEmbed()
     .setTitle(`Stats for ${message.member.displayName}`)
     .addFields(
-      { name: "joined:", value: `${joined_at.toLocaleString()}` },
+      { name: "joined:", value: joined_at.toLocaleString() },
       { name: "messages sent:", value: stats.points },
+      { name: "last message:", value: last_msg.toLocaleString() },
       { name: "level:", value: stats.level }
     )
-    .setImage(avatar);
+    .setImage(avatar)
+    .setColor(color);
 
   message.channel.send(embed);
 };
