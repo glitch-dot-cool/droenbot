988790ecd81bf59a9909ff53ec2cacc3850e62f9
agohorel <3317203+agohorel@users.noreply.github.com>
2020-08-23 18:07:55 -0400

    fix path issue in knexfile

--- db/db-config.js
+++ db/db-config.js
@@ -1,5 +1,5 @@
-const knex = require('knex');
-const knexfile = require('../knexfile.js');
-const env = process.env.DB_ENV || 'development';
+const knex = require("knex");
+const knexfile = require("./knexfile.js");
+const env = process.env.DB_ENV || "development";
 
 module.exports = knex(knexfile[env]);
--- db/knexfile.js
+++ db/knexfile.js
@@ -1,37 +1,39 @@
+const path = require("path");
+
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true, // needed for sqlite
     connection: {
-      filename: './data/droenbot_dev.db3',
+      filename: path.resolve(__dirname, "./data/droenbot_dev.db3"),
     },
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations",
     },
     seeds: {
-      directory: './data/seeds',
+      directory: "./data/seeds",
     },
     pool: {
       afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
       },
     },
   },
   production: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true, // needed for sqlite
     connection: {
-      filename: './data/droenbot.db3',
+      filename: path.resolve(__dirname, "./data/droenbot.db3"),
     },
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations",
     },
     seeds: {
-      directory: './data/seeds',
+      directory: "./data/seeds",
     },
     pool: {
       afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
       },
     },
   },
