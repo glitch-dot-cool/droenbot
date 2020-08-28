9d7ec441d1df9a156747151664d591da72b65d04
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-08-28 11:26:20 -0400

    update role detection to v12, fix arg detection

--- commands/reload.js
+++ commands/reload.js
@@ -1,13 +1,15 @@
 exports.run = (client, message, args) => {
-  const mod = message.guild.roles.find((role) => role.name === "mod");
+  const mod_role = message.member.roles.cache.some(
+    (role) => role.name === "mod"
+  );
 
-  if (!message.member.roles.has(mod.id)) {
+  if (!mod_role) {
     return message.reply(
       "You do not have sufficient permissions to use this command."
     );
   }
 
-  if (!args || args.size < 1)
+  if (!args.length)
     return message.reply("Must provide a command name to reload.");
   delete require.cache[require.resolve(`./${args[0]}.js`)];
   message.reply(`The command ${args[0]} has been reloaded`);
