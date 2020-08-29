fc6f70a3864983743e4093c207836627b53933de
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-08-29 19:43:28 -0400

    add remove helper to infraction-model

--- db/infraction-model.js
+++ db/infraction-model.js
@@ -3,8 +3,25 @@ const db = require("./db-config.js");
 function fetch(userID) {
   return db("users as u")
     .join("infractions as i", { user_fk: "u.id" })
-    .select("u.username", "u.infractions", "i.description", "i.reported_by")
+    .select(
+      "u.username",
+      "u.infractions",
+      "i.description",
+      "i.reported_by",
+      "i.id"
+    )
     .where({ user_fk: userID });
 }
 
-module.exports = { fetch };
+async function remove(infractionID) {
+  const [infraction] = await db("infractions").where({ id: infractionID });
+  const [user] = await db("users").where({ id: infraction.user_fk });
+
+  await db("users")
+    .where({ id: infraction.user_fk })
+    .update({ infractions: user.infractions - 1 });
+
+  await db("infractions").where({ id: infractionID }).delete();
+}
+
+module.exports = { fetch, remove };
