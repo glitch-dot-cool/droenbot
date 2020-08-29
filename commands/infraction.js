4cfc5f5d202ac081fdf5024e00538e0d8175108e
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-08-29 16:21:22 -0400

    add list flag for viewing user infractions

--- commands/infraction.js
+++ commands/infraction.js
@@ -1,30 +1,72 @@
 const db = require("../db/db-model");
+const infractions = require("../db/infraction-model");
+const Discord = require("discord.js");
 
 exports.run = async (bot, message, args) => {
+  const [mode] = args;
+
+  switch (mode) {
+    case "list":
+    case "view":
+      view_infractions(args, message);
+      break;
+
+    default:
+      lodge_infraction(args, message);
+  }
+};
+
+async function view_infractions(args, message) {
+  const [mode, userID] = args;
+
+  const user_infractions = await infractions.fetch(userID);
+  console.log(user_infractions);
+
+  if (user_infractions.length) {
+    const infraction_embed = new Discord.MessageEmbed()
+      .setTitle(`${user_infractions[0].username}'s infractions:`)
+      .addFields(
+        user_infractions.map((infraction) => {
+          return {
+            name: infraction.description,
+            value: `reported by: ${infraction.reported_by}`,
+          };
+        })
+      )
+      .setFooter(`${user_infractions[0].infractions} total infraction(s)`);
+
+    message.channel.send(infraction_embed);
+  } else {
+    message.reply("This user has no infractions!");
+  }
+}
+
+async function lodge_infraction(args, message) {
   const userID = args[0];
   const description = args.slice(1).join(" ");
 
   try {
     // lookup user in infractions table
-    const [user] = await db.findBy("users", { discord_id: userID });
     const [user_infractions] = await db.findBy("infractions", {
-      user_fk: user.id,
+      user_fk: userID,
     });
 
     // if no user_infractions, initialize infraction count
     if (!user_infractions) {
-      await db.update("users", { infractions: 1 }, { id: user.id });
+      await db.update("users", { infractions: 1 }, { id: userID });
     } else {
+      const [user] = await db.findBy("users", { id: userID });
+
       await db.update(
         "users",
         { infractions: user.infractions + 1 },
-        { id: user.id }
+        { id: userID }
       );
     }
 
     const infraction_data = {
-      user_fk: user.id,
-      reported_by: message.author.id,
+      user_fk: userID,
+      reported_by: message.author.username,
       description,
     };
 
@@ -32,6 +74,7 @@ exports.run = async (bot, message, args) => {
 
     message.reply("Infraction successfully lodged");
   } catch (error) {
+    console.error(error);
     message.reply("Error lodging infraction");
   }
-};
+}
