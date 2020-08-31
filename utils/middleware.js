954a57ea15558dcd0e4a9242e082e9862658f7ee
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-08-31 17:40:10 -0400

    add proper sha-1 hash verification to github webhook middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,13 +1,36 @@
+const crypto = require("crypto");
+const bufferEq = require("buffer-equal-constant-time");
 const { github_secret } = require("../config.json");
 
 const validate_github_webhook = (req, res, next) => {
-  const { ref: branch, secret } = req.body;
-  console.log(typeof secret, typeof github_secret, github_secret);
+  const { headers } = req;
+  const { ref: branch } = req.body;
+  const local_hash = hash_secret(github_secret, JSON.stringify(req.body));
+  const remote_hash = `sha1=${headers["x-hub-signature"]}`;
 
-  if (branch.includes("master") && secret === github_secret) {
+  if (branch.includes("master") && verify_signatures(local_hash, remote_hash)) {
     next();
   } else {
     res.status(401).json({ message: "Unauthorized" });
+  }
+};
+
+const hash_secret = (secret, payload) => {
+  return `sha1=${crypto
+    .createHmac("sha1", secret)
+    .update(payload)
+    .digest("hex")}`;
+};
+
+const verify_signatures = (buffer1, buffer2) => {
+  const a = Buffer.from(buffer1);
+  const b = Buffer.from(buffer2);
+
+  if (bufferEq(a, b)) {
+    return true;
+  } else {
+    // unauthorized
+    return false;
   }
 };
 
