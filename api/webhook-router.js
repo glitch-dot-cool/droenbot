7fd918d3cd65295123e94b9c4dd1ec664de1e613
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-09-01 17:03:21 -0400

    update webhook route to asynchronously call the update script

--- api/webhook-router.js
+++ api/webhook-router.js
@@ -1,12 +1,22 @@
+const { exec } = require("child_process");
+const path = require("path");
 const router = require("express").Router();
-const { execSync } = require("child_process");
 const { validate_github_webhook } = require("../utils/middleware");
 
 router.post("/github", validate_github_webhook, async (req, res) => {
   try {
     res.status(200).end();
-    // update bot
-    execSync(`(git pull origin master && forever restart bot.js)`);
+    // run update script
+    exec(
+      `bash ${path.resolve(__dirname, "../scripts/update.sh")}`,
+      (err, stout, sterr) => {
+        if (!err) {
+          res
+            .status(500)
+            .json({ message: "Failed to update bot.", error: sterr });
+        }
+      }
+    );
   } catch (error) {
     res.status(500).json({
       message: "Failed to update and restart bot",
