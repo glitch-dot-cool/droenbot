af5d20a88ac76394d1c2d88d631383fdc8c8bbee
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-09-04 18:15:18 -0400

    treat all role command input as lowercase
    important: all roles within discord must be lowercase

--- commands/role.js
+++ commands/role.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 exports.run = async (bot, message, args) => {
-  const role_name = args.join(" ");
+  const role_name = args.join(" ").toLowerCase();
 
   const restricted_roles = [
     "mod",
