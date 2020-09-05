e93839e90e33958d4fb16a50010312f6042aab1b
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-09-05 15:07:51 -0400

    add optional page and group by pack flags

--- commands/sample.js
+++ commands/sample.js
@@ -7,11 +7,13 @@ exports.run = async (bot, message, args) => {
   const arguments = yargs.parse(args);
   const query = arguments._.join(" ");
   const limit = arguments.limit || 15;
+  const page = arguments.page || 1;
+  const group = arguments.group ? 1 : 0;
 
   const {
     data: { results },
   } = await axios.get(
-    `https://freesound.org/apiv2/search/text/?query=${query}&token=${freesound_token}`
+    `https://freesound.org/apiv2/search/text/?query=${query}&token=${freesound_token}&page=${page}&group_by_pack=${group}`
   );
 
   // make + append url field
@@ -24,10 +26,10 @@ exports.run = async (bot, message, args) => {
 
   const embed = new Discord.MessageEmbed()
     .setTitle("Search Results:")
-    .setDescription("Note: these are *download links*")
+    .setDescription("Try calling `!sample --help` for options")
     .addFields(
       results.slice(0, limit).map((sample) => {
-        return { name: sample.name, value: sample.url };
+        return { name: sample.name, value: `[download](${sample.url})` };
       })
     )
     .setFooter("results from freesound.org");
