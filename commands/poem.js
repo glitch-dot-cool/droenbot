764ef67f6004ce961e9e851baba8d0f21d0a8fb7
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2022-01-15 17:26:55 -0500

    add poem command

---
+++ commands/poem.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+const axios = require("axios");
+
+exports.run = async (bot, message, args) => {
+  const TRUNCATION_MESSAGE = "...[truncated]";
+  const MAX_SIZE = 4096 - TRUNCATION_MESSAGE.length;
+
+  try {
+    const res = await axios.get("https://poetrydb.org/random");
+    const [{ title, author, lines }] = res.data;
+    let poem = lines.join("\r\n");
+
+    if (poem.length > MAX_SIZE) {
+      poem = `${poem.substring(0, MAX_SIZE)}${TRUNCATION_MESSAGE}`;
+    }
+
+    const embed = new Discord.MessageEmbed()
+      .setTitle(title)
+      .setDescription(poem)
+      .addFields(link_to_text(title))
+      .setFooter(`by ${author}`);
+
+    message.channel.send(embed);
+  } catch (error) {
+    message.channel.send("oops, something went wrong");
+  }
+};
+
+function link_to_text(title) {
+  return {
+    name: "Full Text",
+    value: encodeURI(
+      `https://poetrydb.org/title/${title}/author,title,lines.text`
+    ),
+  };
+}
