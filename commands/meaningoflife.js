e9354ee0a387df24cdf003fa3f3311ef8d927bf5
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2022-01-16 22:40:57 -0500

    add meaningoflife command

---
+++ commands/meaningoflife.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+exports.run = (bot, message, args) => {
+  const embed = new Discord.MessageEmbed().setImage(
+    "https://upload.wikimedia.org/wikipedia/commons/5/56/Answer_to_Life.png"
+  );
+
+  message.reply(embed);
+};
