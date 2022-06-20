88fb1194767c76a17386f204f99c03fd93713085
agohorel <alex.gohorel@monthly.com>
2022-06-20 14:33:36 -0400

    add printpaint command

---
+++ commands/printPaint.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+
+exports.run = async (bot, message, args) => {
+  const timestamp = +new Date();
+
+  const embed = new Discord.MessageEmbed()
+    .setTitle("Current state of the canvas 🎨🖌️")
+    .setURL(`https://paint.glitch.cool/image/${timestamp}`)
+    .setImage(`https://paint.glitch.cool/thumbnail/${timestamp}`)
+    .setFooter("Try it out at paint.glitch.cool");
+
+  message.channel.send(embed);
+};
