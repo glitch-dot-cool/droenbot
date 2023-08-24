ade6be2a489e670a42425abfb5a2bf872f4ef5e6
agohorel <alex.gohorel@monthly.com>
2023-08-23 21:06:16 -0400

    fix/update stats command

--- commands/stats.js
+++ commands/stats.js
@@ -3,7 +3,7 @@ const { get_user_message_count } = require("../db/user-model");
 const get_restricted_channels = require("../utils/get_restricted_channels");
 
 exports.run = async (bot, message, args) => {
-  const restricted_channels = get_restricted_channels(message);
+  const restricted_channels = await get_restricted_channels(message);
   const { user, message_details } = await get_user_message_count(
     message.author.id,
     restricted_channels
@@ -15,52 +15,35 @@ exports.run = async (bot, message, args) => {
   const color =
     bot.guilds.resolve(guild_id).members.resolve(author_id)?.roles?.color
       ?.color || "#1a1a1a";
+
   const avatar = bot.guilds
     .resolve(guild_id)
     .members.resolve(author_id)
     .user.avatarURL();
-  const top_channels = message_details.sort(
-    (a, b) => b.total_count - a.total_count
-  );
+
+  const top_channels = message_details
+    .sort((a, b) => b.total_count - a.total_count)
+    // limit to top 5 channels
+    .slice(0, 5);
+
+  const channels_to_display = top_channels.map((channel, idx) => {
+    return {
+      name: `#${idx + 1} channel`,
+      value: `${
+        channel.channel_name
+      } (${channel.total_count.toLocaleString()} msgs)`,
+    };
+  });
 
   const embed = new Discord.MessageEmbed()
     .setTitle(`Stats for ${message.member.displayName}`)
-    .addFields(
+    .addFields([
       { name: "messages sent:", value: user.messages_sent.toLocaleString() },
-      {
-        name: "#1 channel:",
-        value: `${
-          top_channels[0].channel_name
-        } (${top_channels[0].total_count.toLocaleString()} msgs)`,
-      },
-      {
-        name: "#2 channel:",
-        value: `${
-          top_channels[1].channel_name
-        } (${top_channels[1].total_count.toLocaleString()} msgs)`,
-      },
-      {
-        name: "#3 channel:",
-        value: `${
-          top_channels[2].channel_name
-        } (${top_channels[2].total_count.toLocaleString()} msgs)`,
-      },
-      {
-        name: "#4 channel:",
-        value: `${
-          top_channels[3].channel_name
-        } (${top_channels[3].total_count.toLocaleString()} msgs)`,
-      },
-      {
-        name: "#5 channel:",
-        value: `${
-          top_channels[4].channel_name
-        } (${top_channels[4].total_count.toLocaleString()} msgs)`,
-      },
-      { name: "joined:", value: joined_at.toLocaleString() }
-    )
+      ...channels_to_display,
+      { name: "joined:", value: joined_at.toLocaleString() },
+    ])
     .setImage(avatar)
     .setColor(color);
 
-  message.channel.send(embed);
+  message.channel.send({ embeds: [embed] });
 };
